f351327436ab318c196f11f571bf4dd797426a52
galhajaj <galhajaj@gmail.com>
2016-08-31 22:54:49 +0300

    add color tetris to source

---
+++ Assets/Scripts/EscapeToMainScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EscapeToMainScene : MonoBehaviour 
+{
+    public Text ScoreText;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("mainScene");
+        }
+	
+	}
+}
---
+++ Assets/Scripts/GameLoop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLoop : MonoBehaviour 
+{
+    public float StepTime = 0.5F;
+    private float _timePassed = 0.0F;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        _timePassed -= Time.deltaTime;
+        if (_timePassed <= 0.0F)
+        {
+            _timePassed = StepTime;
+
+            // DO THINGS!
+        }
+	}
+}
