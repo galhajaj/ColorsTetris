8e1fb27d0c5688ba07e67fbb6233f4b39af3e1c2
galhajaj <galhajaj@gmail.com>
2016-09-04 00:32:09 +0300

    Revert "Revert "add main screen display""
    This reverts commit 88ce306ab4f028d46a0214bbac8101347ada0ce2.

---
+++ Assets/Scripts/DisplayTile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayTile : MonoBehaviour 
+{
+    public float Speed;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - Speed * Time.deltaTime, this.transform.position.z);
+
+        if (this.transform.position.y <= -6.0F)
+            Destroy(this.gameObject);
+	}
+}
